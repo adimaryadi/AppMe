445c1522f37ef41617c25e6d535936aac627f965
adimaryadi <adimaryadiinten@gmail.com>
2019-02-22 14:49:47 +0700

    search fitur add

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  constructor(private menu: MenuController) {}
+
+  menuOpen() {
+    this.menu.enable(true,'pesan');
+    this.menu.open('pesan');
+  }
+  
+  cari() {
+     let caristyle            =    document.getElementsByClassName('cari') as HTMLCollectionOf<HTMLElement>; 
+     if (caristyle.length != 0) {
+        caristyle[0].style.opacity = "1";
+        caristyle[0].style.visibility = "visible";
+     }
+
+     let btnpencarian         =  document.getElementsByClassName('pencarian') as HTMLCollectionOf<HTMLElement>;
+     if (btnpencarian.length != 0) {
+       btnpencarian[0].style.opacity      =   "0";
+       btnpencarian[0].style.visibility   =   "visibility 0s, opacity 0.5s linear";
+       btnpencarian[0].style.zIndex       =   "0";
+     }
+     let btntutuppencarian    = document.getElementsByClassName('tutuppencarian') as HTMLCollectionOf<HTMLElement>;
+     if (btntutuppencarian.length != 0) {
+      btntutuppencarian[0].style.opacity  =   "1";
+     }
+  }
+
+  tutupcari() {
+    let caristyle           =    document.getElementsByClassName('cari') as HTMLCollectionOf<HTMLElement>;
+    if (caristyle.length != 0) {
+      caristyle[0].style.opacity      =   "0";
+      caristyle[0].style.visibility   =   "visibility 0s, opacity 0.5s linear";
+    }
+    let btnpencarian        =    document.getElementsByClassName('pencarian') as HTMLCollectionOf<HTMLElement>;
+    if (btnpencarian.length != 0) {
+      btnpencarian[0].style.opacity   =   "1";
+      btnpencarian[0].style.zIndex    =   "1";
+      btnpencarian[0].style.visibility=   "visibility 0s, opacity 0.5s linear";
+    }
+    let btntutuppencarian   =   document.getElementsByClassName('tutuppencarian') as HTMLCollectionOf<HTMLElement>;
+    if (btntutuppencarian.length != 0) {
+      btntutuppencarian[0].style.opacity    =   "0";
+    }
+  }
+}
